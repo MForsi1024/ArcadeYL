590dcbac3111d815ba103a6db5d427fdea5c00c2
Каракозов Матвей <matveykax@yandex.ru>
2026-01-09 18:33:52 +0400

    Включено отображение во весь экран

--- main.py
+++ main.py
@@ -56,7 +56,13 @@ class GameMenu(UI):
 
 
 if __name__ == '__main__':
-    window = arcade.Window(800, 600, "Учимся ставить на паузу")
+    print(arcade.get_display_size())
+    width, height = arcade.get_display_size()
+    window = arcade.Window(
+        width=width,
+        height=height,
+        title="Подводная битва")
+    window.set_location(0, 0)
     menu_view = StartMenu()
     window.show_view(menu_view)
     arcade.run()
