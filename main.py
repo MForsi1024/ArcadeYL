b303203b9118654a394ebaee5736135a7b29bba0
Каракозов Матвей <matveykax@yandex.ru>
2026-01-09 17:41:11 +0400

    Добавлено тестовое главное меню

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import arcade
+from pyglet.graphics import Batch
 
-class UI:
-    pass
+class UI(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.background_color = arcade.color.BLUE_GRAY  # Фон для меню
+
+        self.batch = Batch()
+        self.main_text = arcade.Text("Главное Меню", self.window.width / 2, self.window.height / 2 + 50,
+                                     arcade.color.WHITE, font_size=40, anchor_x="center", batch=self.batch)
+        self.space_text = arcade.Text("Нажми SPACE, чтобы начать!", self.window.width / 2, self.window.height / 2 - 50,
+                                      arcade.color.WHITE, font_size=20, anchor_x="center", batch=self.batch)
+
+
+    def on_draw(self):
+        self.clear()
+        self.batch.draw()
 
 
 class StartMenu(UI):
@@ -10,3 +24,9 @@ class StartMenu(UI):
 
 class GameMenu(UI):
     pass
+
+if __name__ == '__main__':
+    window = arcade.Window(800, 600, "Учимся ставить на паузу")
+    menu_view = UI()
+    window.show_view(menu_view)
+    arcade.run()
