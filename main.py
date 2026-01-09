8600f1c5d11cad76e796a7b9002e7f1ce6f1a1fe
Каракозов Матвей <matveykax@yandex.ru>
2026-01-09 18:01:45 +0400

    Создано отображение стартового экрана

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import arcade
 from arcade.gui import UIFlatButton, UIBoxLayout, UIAnchorLayout, UIManager
 from pyglet.graphics import Batch
 
+
 class UI(arcade.View):
     def __init__(self):
         super().__init__()
         self.manager = UIManager()
         self.manager.enable()
-        self.background_color = arcade.color.BLUE_GRAY  # Фон для меню
         self.batch = Batch()
 
     def on_draw(self):
@@ -19,6 +19,7 @@ class UI(arcade.View):
 class StartMenu(UI):
     def __init__(self):
         super().__init__()
+        self.background_color = arcade.color.BLUE_GRAY  # Фон для меню
         self.anchor_layout = UIAnchorLayout()
         self.box_layout = UIBoxLayout(vertical=True, space_between=10)
 
@@ -28,9 +29,12 @@ class StartMenu(UI):
         self.manager.add(self.anchor_layout)
 
     def setup_widgets(self):
-        self.flat_button = UIFlatButton(text="Плоская Кнопка", width=200, height=50, color=arcade.color.BLUE)
-        self.flat_button.on_click = lambda event: print("Flat клик!")  # Не только лямбду, конечно
-        self.box_layout.add(self.flat_button)
+        flat_button = UIFlatButton(text="Плоская Кнопка", width=200, height=50, color=arcade.color.BLUE)
+        flat_button.on_click = lambda event: print("Flat клик!")  # Не только лямбду, конечно
+        self.box_layout.add(flat_button)
+
+    def on_draw(self):
+        super().on_draw()
 
 
 class GameMenu(UI):
@@ -39,6 +43,6 @@ class GameMenu(UI):
 
 if __name__ == '__main__':
     window = arcade.Window(800, 600, "Учимся ставить на паузу")
-    menu_view = UI()
+    menu_view = StartMenu()
     window.show_view(menu_view)
     arcade.run()
