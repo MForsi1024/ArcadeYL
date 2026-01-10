6ae2c0f2d8a58d95b0fb6363d8f873f262013243
Boss_of_pentest228 <magaygregory@gmail.com>
2026-01-10 21:57:00 +0400

    Маленькие косметические изменения (Главное меню 1)

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class StartMenu(UI):
     def __init__(self):
         super().__init__()
         self.menu_text = arcade.Text("Подводная битва", self.window.width / 2, self.window.height * 0.75,
-                                      arcade.color.WHITE, font_size=40, anchor_x="center", batch=self.batch)
+                                     arcade.color.WHITE, font_size=40, anchor_x="center", batch=self.batch)
         self.background_color = arcade.color.BLUE_GRAY  # Фон для меню
         self.anchor_layout = UIAnchorLayout()
         self.box_layout = UIBoxLayout(vertical=True, space_between=10)
@@ -31,14 +31,20 @@ class StartMenu(UI):
         self.manager.add(self.anchor_layout)
 
     def setup_widgets(self):
+        button_style = {
+            "normal": {"font_size": 25},
+            "hover": {"font_size": 25},
+            "press": {"font_size": 25},
+            "disabled": {"font_size": 25}
+        }
 
-        start_button = UIFlatButton(text="Начать игру", width=200, height=50, color=arcade.color.BLUE)
+        start_button = UIFlatButton(text="Начать игру", width=450, height=100, color=arcade.color.BLUE, style=button_style)
         start_button.on_click = lambda event: print("Flat клик!")
 
-        settings_button = UIFlatButton(text="Настройки", width=200, height=50, color=arcade.color.BLUE)
+        settings_button = UIFlatButton(text="Настройки", width=450, height=100, color=arcade.color.BLUE, style=button_style)
         settings_button.on_click = lambda event: print("Flat клик!")
 
-        exit_button = UIFlatButton(text="Выйти из игры", width=200, height=50, color=arcade.color.BLUE)
+        exit_button = UIFlatButton(text="Выйти из игры", width=450, height=100, color=arcade.color.BLUE, style=button_style)
         exit_button.on_click = lambda event: arcade.close_window()
 
         self.box_layout.add(start_button)
@@ -47,8 +53,6 @@ class StartMenu(UI):
 
     def on_draw(self):
         super().on_draw()
-
-
 
 
 class GameMenu(UI):
