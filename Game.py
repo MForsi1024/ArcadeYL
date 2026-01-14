1540563dd8abeb96625153e8962facc29752dda6
Boss_of_pentest228 <magaygregory@gmail.com>
2026-01-14 19:19:06 +0400

    Отображение фона

--- Game.py
+++ Game.py
@@ -6,4 +6,11 @@ from UI import UI
 class GlobalMain(UI):
     def __init__(self):
         super().__init__()
-        self.background = arcade.load_image("resources/images/arcade_test_background.png")
+        self.background = arcade.load_texture("resources/images/arcade_test_background.png")
+
+    def on_draw(self):
+        self.clear()
+        arcade.draw_texture_rect(
+            self.background,
+            arcade.rect.XYWH(self.width // 2, self.height // 2, self.width, self.height,)
+        )
