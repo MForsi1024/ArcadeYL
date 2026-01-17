eb83d00896b5b05c7a4236041f98c5e481dd4ac0
Каракозов Матвей <matveykax@yandex.ru>
2026-01-17 20:32:46 +0400

    Добавлен requirements.txt и начата работа над сохранениями

--- Saves.py
+++ Saves.py
@@ -1,5 +1,24 @@
-class Reader:
+class Saves:
     pass
 
-class Writer:
-    pass
+
+class FileManager:
+    """Файловый менеджер, который необходим для работы с файлами"""
+    def __init__(self, file):
+        self.file = file
+
+
+class Reader(FileManager):
+    def read_levels(self):
+        with open(self.file, mode='r', encoding='utf-8') as f:
+            pass
+class Writer(FileManager):
+    def save_levels(self, levels, times):
+        try:
+            with open(self.file, mode='r', encoding='utf-8') as f:
+                for i, j in zip(levels, times):
+                    f.write(f'{i},{j}')
+                return True
+        except FileNotFoundError or FileExistsError:
+            return False
+
