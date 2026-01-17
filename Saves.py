4b6fc69da9468931b5f2e2ec250f1d8363e36290
Каракозов Матвей <matveykax@yandex.ru>
2026-01-17 20:57:58 +0400

    Доработано сохранение данных

--- Saves.py
+++ Saves.py
@@ -1,19 +1,36 @@
-class Saves:
-    pass
-
-
 class FileManager:
     """Файловый менеджер, который необходим для работы с файлами"""
+
     def __init__(self, file):
         self.file = file
 
+    def get_levels(self):
+        reader = Reader(self.file)
+        return reader.read_levels()
+
+    def save_levels(self, levels, times):
+        writer = Writer(self.file)
+        return writer.write_levels(levels, times)
+
 
 class Reader(FileManager):
+    """Класс для чтения данных. Возвращает True в случае успеха и False - неудачи."""
+
     def read_levels(self):
-        with open(self.file, mode='r', encoding='utf-8') as f:
-            pass
+        try:
+            with open(self.file, mode='r', encoding='utf-8') as f:
+                lt = f.readline().split(';')
+                levels = [i[0] for i in lt]
+                times = [i[1] for i in lt]
+                return levels, times
+        except FileNotFoundError or FileExistsError:
+            return False
+
+
 class Writer(FileManager):
-    def save_levels(self, levels, times):
+    """Класс для сохранения данных. Возвращает True в случае успеха и False - неудачи."""
+
+    def write_levels(self, levels, times):
         try:
             with open(self.file, mode='r', encoding='utf-8') as f:
                 for i, j in zip(levels, times):
@@ -21,4 +38,3 @@ class Writer(FileManager):
                 return True
         except FileNotFoundError or FileExistsError:
             return False
-
